0eae5bd10b34c00720706566bc6528f584a0f6e0
Nathan Wing <NathanWing@tfl.gov.uk>
2025-04-23 09:35:28 +0100

    feature: add task logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿Console.WriteLine("Task Tracker");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Task newTask = new Task();
+        List<Task> tasks = new List<Task>();
 
+        if (args[0] == "add")
+        {
+            newTask.TaskName = args[1];
+            newTask.TaskId = tasks.Count + 1;
+            tasks.Add(newTask);
+        }
+
+        //Output
+        Console.WriteLine($"Task added successfully (ID:{newTask.TaskId})");
+    }
+}
+
+public class Task
+{
+    public string TaskName { get; set; }
+    public int TaskId { get; set; }
+
+}
