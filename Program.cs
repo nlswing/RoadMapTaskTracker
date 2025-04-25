16e3fa842588ffd46f3efa8f58bae20b31e99150
Nathan Wing <nathanwing@tfl.gov.uk>
2025-04-25 11:11:47 +0100

    feat:  store added task to JSON

--- Program.cs
+++ Program.cs
@@ -1,21 +1,26 @@
-﻿class Program
+﻿using System.Text.Json;
+
+string appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TaskTracker");
+Directory.CreateDirectory(appDataPath);
+string jsonFilePath = Path.Combine(appDataPath, "tasks.json");
+
+Task newTask = new Task();
+List<Task> tasks = new List<Task>();
+
+if (args[0] == "add")
 {
-    static void Main(string[] args)
-    {
-        Task newTask = new Task();
-        List<Task> tasks = new List<Task>();
-
-        if (args[0] == "add")
-        {
-            newTask.TaskName = args[1];
-            newTask.TaskId = tasks.Count + 1;
-            tasks.Add(newTask);
-        }
-
-        //Output
-        Console.WriteLine($"Task added successfully (ID:{newTask.TaskId})");
-    }
+    newTask.TaskName = args[1];
+    newTask.TaskId = tasks.Count + 1;
+    tasks.Add(newTask);
 }
+
+var options = new JsonSerializerOptions();
+options.WriteIndented = true;
+string jsonString = JsonSerializer.Serialize(tasks, options);
+File.WriteAllText(jsonFilePath, jsonString);
+
+//Output
+Console.WriteLine($"Task added successfully (ID:{newTask.TaskId})");
 
 public class Task
 {
